43bb7530eea91f609fb51b267a7c16c816b0e3ce
ajlankp848 <ajlantkm848@gmail.com>
2021-01-28 11:16:36 +0530

    sample python

---
+++ flowControls/decisionmaking/leval-two.py
@@ -0,0 +1,5 @@
+num=int(input("enter a number"))
+if(num>0):
+    print("the entered number is positive")
+else:
+    print("the entered number is negative")
---
+++ flowControls/decisionmaking/level-four.py
@@ -0,0 +1,6 @@
+num1=int(input("enter first numer"))
+num2=int(input("enter second number"))
+if(num1>num2):
+    print("the largest number is ", num1)
+else:
+    print("the largest number is ", num2)
